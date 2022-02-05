d03475b8f4b2b411fae60c5b4888eb48ba58f653
info@coders.VN <CodersTokyo>
2022-02-05 17:41:34 +0700

    fix ..

--- Manage.java
+++ Manage.java
@@ -1,10 +1,9 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-
 public class Manage {
     public static final int ROOM_OF_NUMBERS = 20;
     private List<Room> rooms;
-
     public Manage(){
         rooms = new ArrayList<>();
     }
@@ -12,20 +11,25 @@ public class Manage {
         this.rooms.add(room);
     }
 
-    public Boolean deleteInformationOfPersonByPassport(int roomNumber){
+    public void deleteInformationOfPersonInRoom(int roomNumber){
         Room room = this.rooms.stream().filter(o -> o.getRoomNumber() == roomNumber).findFirst().orElse(null);
-
-        if(room == null){
-            return false;
+        if(room != null){
+            System.out.println("Room you have just removed information of customer: "+room.toString());
+            room.deletePersonByRoom();
         }
-        else {
-            this.rooms.remove(room.)
-        }
+        else System.out.println("invalid");
     }
+
+
+
+    public void deleteInformationOfCustomerByPassport(String passport){
+        Room room = this.rooms.stream().filter(o -> o.getPersons().stream().filter(i -> i.getPassport() == passport)).findFirst().orElse(null);
+
+    }
+
     @Override
     public String toString() {
         return "information: " + rooms.toString();
     }
-
 
 }
